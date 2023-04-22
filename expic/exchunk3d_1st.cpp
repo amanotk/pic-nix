5ff804d49b96981e0ccb81912705f8645fc68c80
Takanobu Amano <amano@eps.s.u-tokyo.ac.jp>
2023-04-22 17:16:46 +0900

    Fix to follow update in nix

--- expic/exchunk3d_1st.cpp
+++ expic/exchunk3d_1st.cpp
@@ -8,6 +8,13 @@
   template <>                                                                                      \
   type ExChunk3D<1>::name
 
+using nix::digitize;
+using nix::lorentz_factor;
+using nix::push_buneman_boris;
+using nix::shape1;
+using nix::interp3d1;
+using nix::esirkepov3d1;
+
 DEFINE_MEMBER(void, push_velocity)(const float64 delt)
 {
   const float64 rc    = 1 / cc;
@@ -36,24 +43,24 @@ DEFINE_MEMBER(void, push_velocity)(const float64 delt)
       float64 emf[6] = {0};
 
       float64* xu  = &ps->xu(ip, 0);
-      float64  gam = Particle::lorentz_factor(xu[3], xu[4], xu[5], rc);
+      float64  gam = lorentz_factor(xu[3], xu[4], xu[5], rc);
       float64  dt2 = dt1 * rc / gam;
 
       // grid indices
-      int ix = Particle::digitize(xu[0], ximin, rdx);
-      int hx = Particle::digitize(xu[0], xhmin, rdx);
-      int iy = Particle::digitize(xu[1], yimin, rdy);
-      int hy = Particle::digitize(xu[1], yhmin, rdy);
-      int iz = Particle::digitize(xu[2], zimin, rdz);
-      int hz = Particle::digitize(xu[2], zhmin, rdz);
+      int ix = digitize(xu[0], ximin, rdx);
+      int hx = digitize(xu[0], xhmin, rdx);
+      int iy = digitize(xu[1], yimin, rdy);
+      int hy = digitize(xu[1], yhmin, rdy);
+      int iz = digitize(xu[2], zimin, rdz);
+      int hz = digitize(xu[2], zhmin, rdz);
 
       // weights
-      Particle::S1(xu[0], ximin + ix * delx, rdx, wix);
-      Particle::S1(xu[0], xhmin + hx * delx, rdx, whx);
-      Particle::S1(xu[1], yimin + iy * dely, rdy, wiy);
-      Particle::S1(xu[1], yhmin + hy * dely, rdy, why);
-      Particle::S1(xu[2], zimin + iz * delz, rdz, wiz);
-      Particle::S1(xu[2], zhmin + hz * delz, rdz, whz);
+      shape1(xu[0], ximin + ix * delx, rdx, wix);
+      shape1(xu[0], xhmin + hx * delx, rdx, whx);
+      shape1(xu[1], yimin + iy * dely, rdy, wiy);
+      shape1(xu[1], yhmin + hy * dely, rdy, why);
+      shape1(xu[2], zimin + iz * delz, rdz, wiz);
+      shape1(xu[2], zhmin + hz * delz, rdz, whz);
 
       //
       // calculate electromagnetic field at particle position
@@ -71,15 +78,15 @@ DEFINE_MEMBER(void, push_velocity)(const float64 delt)
       hx += Lbx;
       hy += Lby;
       hz += Lbz;
-      emf[0] = Particle::interp3d1(uf, iz, iy, hx, 0, wiz, wiy, whx, dt1);
-      emf[1] = Particle::interp3d1(uf, iz, hy, ix, 1, wiz, why, wix, dt1);
-      emf[2] = Particle::interp3d1(uf, hz, iy, ix, 2, whz, wiy, wix, dt1);
-      emf[3] = Particle::interp3d1(uf, hz, hy, ix, 3, whz, why, wix, dt2);
-      emf[4] = Particle::interp3d1(uf, hz, iy, hx, 4, whz, wiy, whx, dt2);
-      emf[5] = Particle::interp3d1(uf, iz, hy, hx, 5, wiz, why, whx, dt2);
+      emf[0] = interp3d1(uf, iz, iy, hx, 0, wiz, wiy, whx, dt1);
+      emf[1] = interp3d1(uf, iz, hy, ix, 1, wiz, why, wix, dt1);
+      emf[2] = interp3d1(uf, hz, iy, ix, 2, whz, wiy, wix, dt1);
+      emf[3] = interp3d1(uf, hz, hy, ix, 3, whz, why, wix, dt2);
+      emf[4] = interp3d1(uf, hz, iy, hx, 4, whz, wiy, whx, dt2);
+      emf[5] = interp3d1(uf, iz, hy, hx, 5, wiz, why, whx, dt2);
 
       // push particle velocity
-      Particle::push_buneman_boris(&xu[3], emf);
+      push_buneman_boris(&xu[3], emf);
     }
   }
 }
@@ -95,7 +102,7 @@ DEFINE_MEMBER(void, push_position)(const float64 delt)
     for (int ip = 0; ip < ps->Np; ip++) {
       float64* xu  = &ps->xu(ip, 0);
       float64* xv  = &ps->xv(ip, 0);
-      float64  gam = Particle::lorentz_factor(xu[3], xu[4], xu[5], rc);
+      float64  gam = lorentz_factor(xu[3], xu[4], xu[5], rc);
       float64  dt  = delt / gam;
 
       // substitute to temporary
@@ -143,32 +150,32 @@ DEFINE_MEMBER(void, deposit_current)(const float64 delt)
       // -*- weights before move -*-
       //
       // grid indices
-      int ix0 = Particle::digitize(xv[0], ximin, rdx);
-      int iy0 = Particle::digitize(xv[1], yimin, rdy);
-      int iz0 = Particle::digitize(xv[2], zimin, rdz);
+      int ix0 = digitize(xv[0], ximin, rdx);
+      int iy0 = digitize(xv[1], yimin, rdy);
+      int iz0 = digitize(xv[2], zimin, rdz);
 
       // weights
-      Particle::S1(xv[0], ximin + ix0 * delx, rdx, &ss[0][0][1]);
-      Particle::S1(xv[1], yimin + iy0 * dely, rdy, &ss[0][1][1]);
-      Particle::S1(xv[2], zimin + iz0 * delz, rdz, &ss[0][2][1]);
+      shape1(xv[0], ximin + ix0 * delx, rdx, &ss[0][0][1]);
+      shape1(xv[1], yimin + iy0 * dely, rdy, &ss[0][1][1]);
+      shape1(xv[2], zimin + iz0 * delz, rdz, &ss[0][2][1]);
 
       //
       // -*- weights after move -*-
       //
       // grid indices
-      int ix1 = Particle::digitize(xu[0], ximin, rdx);
-      int iy1 = Particle::digitize(xu[1], yimin, rdy);
-      int iz1 = Particle::digitize(xu[2], zimin, rdz);
+      int ix1 = digitize(xu[0], ximin, rdx);
+      int iy1 = digitize(xu[1], yimin, rdy);
+      int iz1 = digitize(xu[2], zimin, rdz);
 
       // weights
-      Particle::S1(xu[0], ximin + ix1 * delx, rdx, &ss[1][0][1 + ix1 - ix0]);
-      Particle::S1(xu[1], yimin + iy1 * dely, rdy, &ss[1][1][1 + iy1 - iy0]);
-      Particle::S1(xu[2], zimin + iz1 * delz, rdz, &ss[1][2][1 + iz1 - iz0]);
+      shape1(xu[0], ximin + ix1 * delx, rdx, &ss[1][0][1 + ix1 - ix0]);
+      shape1(xu[1], yimin + iy1 * dely, rdy, &ss[1][1][1 + iy1 - iy0]);
+      shape1(xu[2], zimin + iz1 * delz, rdz, &ss[1][2][1 + iz1 - iz0]);
 
       //
       // -*- accumulate current via density decomposition -*-
       //
-      Particle::esirkepov3d1(dxdt, dydt, dzdt, ss, cur);
+      esirkepov3d1(dxdt, dydt, dzdt, ss, cur);
 
       ix0 += Lbx;
       iy0 += Lby;
@@ -218,21 +225,21 @@ DEFINE_MEMBER(void, deposit_moment)()
       float64* xu = &ps->xu(ip, 0);
 
       // grid indices
-      int ix = Particle::digitize(xu[0], ximin, rdx);
-      int iy = Particle::digitize(xu[1], yimin, rdy);
-      int iz = Particle::digitize(xu[2], zimin, rdz);
+      int ix = digitize(xu[0], ximin, rdx);
+      int iy = digitize(xu[1], yimin, rdy);
+      int iz = digitize(xu[2], zimin, rdz);
 
       // weights
-      Particle::S1(xu[0], ximin + ix * delx, rdx, wx);
-      Particle::S1(xu[1], yimin + iy * dely, rdy, wy);
-      Particle::S1(xu[2], zimin + iz * delz, rdz, wz);
+      shape1(xu[0], ximin + ix * delx, rdx, wx);
+      shape1(xu[1], yimin + iy * dely, rdy, wy);
+      shape1(xu[2], zimin + iz * delz, rdz, wz);
 
       // deposit to local array (this step is not necessary for scalar version)
       for (int jz = 0; jz < 2; jz++) {
         for (int jy = 0; jy < 2; jy++) {
           for (int jx = 0; jx < 2; jx++) {
             float64 ww = ms * wz[jz] * wy[jy] * wx[jx];
-            float64 gm = Particle::lorentz_factor(xu[3], xu[4], xu[5], rc);
+            float64 gm = lorentz_factor(xu[3], xu[4], xu[5], rc);
 
             //
             //  0: mass density
