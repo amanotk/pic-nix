4d589b8144628b89c6a9c35c81758adaf433f4ec
Takanobu Amano <amano@eps.s.u-tokyo.ac.jp>
2024-04-22 00:44:55 +0900

    Configuration on Fugaku to set MPI_THREAD_MULTIPLE=OFF

--- cmake/fugaku-gcc-cross.cmake
+++ cmake/fugaku-gcc-cross.cmake
@@ -14,14 +14,18 @@
 # export INCLUDE="$FJMPI/include/mpi/fujitsu:$INCLUDE"
 #
 # 3) Load the setup script with the following command:
-# 
+#
 # $ . ${HOME}/setup-gcc-10-aarch64.sh
 #
 set(CMAKE_SYSTEM_NAME Fugaku)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(MPI_THREAD_MULTIPLE OFF CACHE BOOL "MPI thread multiple" FORCE)
+set(GCC10_LIB "/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/gcc-8.5.0/gcc-10.4.0-vuhiczfvddfc5n3tmny2e4ffde6cvsi5/lib64")
 
 # cross compiler on login node
 set(CMAKE_CXX_COMPILER "g++" CACHE FILEPATH "C++ compiler")
-set(CMAKE_CXX_FLAGS "-march=armv8-a+sve -msve-vector-bits=512 -fopenmp -O3" CACHE STRING "C++ compiler flags")
-set(CMAKE_EXE_LINKER_FLAGS "-L/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/gcc-8.5.0/gcc-10.4.0-vuhiczfvddfc5n3tmny2e4ffde6cvsi5/lib64 -lstdc++" CACHE STRING "Linker flags")
+set(CMAKE_CXX_FLAGS "-Wno-psabi \
+    -march=armv8-a+sve -msve-vector-bits=512 -fopenmp -O3"
+    CACHE STRING "C++ compiler flags")
+set(CMAKE_EXE_LINKER_FLAGS "-L${GCC10_LIB} -lstdc++" CACHE STRING "Linker flags")
 set(CMAKE_CXX_LINK_EXECUTABLE "mpiFCCpx <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>" CACHE STRING "Linker")
--- cmake/fugaku-llvm-cross.cmake
+++ cmake/fugaku-llvm-cross.cmake
@@ -7,7 +7,10 @@
 #
 set(CMAKE_SYSTEM_NAME Fugaku)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(MPI_THREAD_MULTIPLE OFF CACHE BOOL "MPI thread multiple" FORCE)
 
 set(CMAKE_CXX_COMPILER "mpiclang++" CACHE FILEPATH "C++ compiler")
-set(CMAKE_CXX_FLAGS "-march=armv8-a+sve -msve-vector-bits=512 -fopenmp -O3" CACHE STRING "C++ compiler flags")
+set(CMAKE_CXX_FLAGS "-Wno-unused-command-line-argument \
+    -march=armv8-a+sve -msve-vector-bits=512 -fopenmp -O3"
+    CACHE STRING "C++ compiler flags")
 set(CMAKE_EXE_LINKER_FLAGS "-L/vol0004/apps/oss/llvm-v14.0.1/login_node/lib" CACHE STRING "Linker flags")
