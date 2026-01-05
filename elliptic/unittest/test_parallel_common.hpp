bbeb4f551113a8bb7694fe097c3bcab7f660f611
Takanobu Amano <amano@eps.s.u-tokyo.ac.jp>
2026-01-05 13:26:04 +0900

    Code cleanup

---
+++ elliptic/unittest/test_parallel_common.hpp
@@ -0,0 +1,244 @@
+#ifndef _TEST_PARALLEL_COMMON_HPP_
+#define _TEST_PARALLEL_COMMON_HPP_
+
+#include "nix.hpp"
+#include "xtensorall.hpp"
+#include <algorithm>
+#include <vector>
+
+using namespace nix;
+using namespace nix::typedefs;
+
+class MockChunk;
+using MockChunkPtr = std::unique_ptr<MockChunk>;
+using MockChunkVec = std::vector<std::unique_ptr<MockChunk>>;
+
+// MockChunk class for testing
+class MockChunk
+{
+protected:
+  int                     Nb;
+  int                     Lbx;
+  int                     Ubx;
+  int                     Lby;
+  int                     Uby;
+  int                     Lbz;
+  int                     Ubz;
+  xt::xtensor<float64, 4> uj;
+
+  // Members mimicking nix::Chunk
+  int dims[3];
+  int id;
+  int offset[3];
+  int global_dims[3];
+
+public:
+  struct DataContainer {
+    int&                     Lbx;
+    int&                     Ubx;
+    int&                     Lby;
+    int&                     Uby;
+    int&                     Lbz;
+    int&                     Ubz;
+    xt::xtensor<float64, 4>& uj;
+  };
+
+  MockChunk() : Nb(2), id(0)
+  {
+    std::fill(std::begin(dims), std::end(dims), 0);
+    std::fill(std::begin(offset), std::end(offset), 0);
+    std::fill(std::begin(global_dims), std::end(global_dims), 0);
+
+    // Minimal initialization
+    uj.resize({(size_t)2 * Nb, (size_t)2 * Nb, (size_t)2 * Nb, 4});
+    uj.fill(0);
+  }
+
+  MockChunk(const int dims[3], int id = 0) : Nb(2), id(id)
+  {
+    std::copy(dims, dims + 3, this->dims);
+    std::fill(std::begin(offset), std::end(offset), 0);
+    std::fill(std::begin(global_dims), std::end(global_dims), 0);
+
+    size_t nz = dims[2] + 2 * Nb;
+    size_t ny = dims[1] + 2 * Nb;
+    size_t nx = dims[0] + 2 * Nb;
+
+    Lbz = Nb;
+    Ubz = Lbz + dims[2] - 1;
+    Lby = Nb;
+    Uby = Lby + dims[1] - 1;
+    Lbx = Nb;
+    Ubx = Lbx + dims[0] - 1;
+
+    uj.resize({nz, ny, nx, 4});
+    uj.fill(0);
+  }
+
+  void set_dims(const std::vector<int>& d)
+  {
+    if (d.size() < 3)
+      return;
+    std::copy(d.begin(), d.begin() + 3, dims);
+
+    size_t nz = dims[2] + 2 * Nb;
+    size_t ny = dims[1] + 2 * Nb;
+    size_t nx = dims[0] + 2 * Nb;
+
+    Lbz = Nb;
+    Ubz = Lbz + dims[2] - 1;
+    Lby = Nb;
+    Uby = Lby + dims[1] - 1;
+    Lbx = Nb;
+    Ubx = Lbx + dims[0] - 1;
+
+    uj.resize({nz, ny, nx, 4});
+    uj.fill(0);
+  }
+
+  void set_offset(const std::vector<int>& o)
+  {
+    if (o.size() < 3)
+      return;
+    std::copy(o.begin(), o.begin() + 3, offset);
+  }
+
+  // Mimic nix::Chunk interface required by elliptic::Solver
+  void set_global_context(const int offset[3], const int global_dims[3])
+  {
+    std::copy(offset, offset + 3, this->offset);
+    std::copy(global_dims, global_dims + 3, this->global_dims);
+  }
+
+  const int* get_dims() const
+  {
+    return dims;
+  }
+
+  const int* get_offset() const
+  {
+    return offset;
+  }
+
+  DataContainer get_internal_data()
+  {
+    return DataContainer{Lbx, Ubx, Lby, Uby, Lbz, Ubz, uj};
+  }
+
+  xt::xtensor<float64, 4>& get_uj()
+  {
+    return uj;
+  }
+
+  void set_id(int new_id)
+  {
+    id = new_id;
+  }
+
+  int get_id() const
+  {
+    return id;
+  }
+};
+
+inline int get_mpi_size()
+{
+  int size;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  return size;
+}
+
+inline int get_mpi_rank()
+{
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  return rank;
+}
+
+inline auto get_rank_dims(const std::array<int, 3>& chunk_dims,
+                          const std::array<int, 3>& global_dims)
+{
+  return std::array<int, 3>{global_dims[0] / chunk_dims[0], global_dims[1] / chunk_dims[1],
+                            global_dims[2] / chunk_dims[2]};
+}
+
+inline auto get_chunklist(int rank, const std::array<int, 3>& rank_dims, int num_chunks_per_rank)
+{
+  std::vector<int> id;
+  std::vector<int> oz;
+  std::vector<int> oy;
+  std::vector<int> ox;
+
+  id.reserve(static_cast<size_t>(num_chunks_per_rank));
+  oz.reserve(static_cast<size_t>(num_chunks_per_rank));
+  oy.reserve(static_cast<size_t>(num_chunks_per_rank));
+  ox.reserve(static_cast<size_t>(num_chunks_per_rank));
+
+  const int nx = rank_dims[2];
+  const int ny = rank_dims[1];
+
+  // global chunk id = cx*sx + cy*sy + cz*sz
+  const int sx = 1;
+  const int sy = nx;
+  const int sz = nx * ny;
+
+  for (int i = 0; i < num_chunks_per_rank; ++i) {
+    const int id_global = rank * num_chunks_per_rank + i;
+
+    // chunk coordinates
+    const int cx = (id_global / sx) % nx;
+    const int cy = (id_global / sy) % ny;
+    const int cz = (id_global / sz);
+
+    id.push_back(id_global);
+    oz.push_back(cz);
+    oy.push_back(cy);
+    ox.push_back(cx);
+  }
+
+  return std::make_tuple(std::move(id), std::move(oz), std::move(oy), std::move(ox));
+}
+
+inline auto get_chunkvec(const std::array<int, 3>& dims, const std::vector<int>& id,
+                         const std::vector<int>& oz, const std::vector<int>& oy,
+                         const std::vector<int>& ox)
+{
+  std::vector<std::shared_ptr<MockChunk>> chunkvec;
+  chunkvec.reserve(id.size());
+
+  for (size_t i = 0; i < id.size(); ++i) {
+    auto chunk = std::make_shared<MockChunk>();
+    chunk->set_id(id[i]);
+    chunk->set_dims({dims[0], dims[1], dims[2]});
+    chunk->set_offset({oz[i] * dims[0], oy[i] * dims[1], ox[i] * dims[2]});
+    chunkvec.push_back(std::move(chunk));
+  }
+
+  return chunkvec;
+}
+
+inline auto get_chunkvec(int rank, const std::array<int, 3>& chunk_dims,
+                         const std::array<int, 3>& rank_dims, int num_chunks_per_rank)
+{
+  const auto [cids, offz, offy, offx] = get_chunklist(rank, rank_dims, num_chunks_per_rank);
+  return get_chunkvec(chunk_dims, cids, offz, offy, offx);
+}
+
+inline auto get_index_and_chunkvec(int rank, const std::array<int, 3>& chunk_dims,
+                                   const std::array<int, 3>& global_dims, int num_chunks_per_rank)
+{
+  const auto rank_dims = get_rank_dims(chunk_dims, global_dims);
+
+  // check consistency
+  const int size = get_mpi_size();
+  assert(size * num_chunks_per_rank == (rank_dims[0] * rank_dims[1] * rank_dims[2]));
+
+  const int chunk_size = chunk_dims[0] * chunk_dims[1] * chunk_dims[2];
+
+  std::vector<int> index(chunk_size * num_chunks_per_rank);
+  auto             chunkvec = get_chunkvec(rank, chunk_dims, rank_dims, num_chunks_per_rank);
+
+  return std::make_tuple(std::move(index), std::move(chunkvec));
+}
+
+#endif //_TEST_PARALLEL_COMMON_HPP_
--- elliptic/unittest/test_petsc_utils.cpp
+++ elliptic/unittest/test_petsc_utils.cpp
@@ -8,114 +8,14 @@
 #include <utility> // std::pair
 #include <vector>
 
-#include <petscao.h>   // AO, AOApplicationToPetsc
-#include <petscdmda.h> // DMDAGetLocalInfo
+#include <petscao.h>
+#include <petscdmda.h>
 
-#include "mock_chunk.hpp"
 #include "petsc_utils.hpp"
+#include "test_parallel_common.hpp"
 
 using namespace nix::typedefs;
 using elliptic::PetscUtils;
-
-inline int get_mpi_size()
-{
-  int size;
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  return size;
-}
-
-inline int get_mpi_rank()
-{
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  return rank;
-}
-
-inline auto get_rank_dims(const std::array<int, 3>& chunk_dims,
-                          const std::array<int, 3>& global_dims)
-{
-  return std::array<int, 3>{global_dims[0] / chunk_dims[0], global_dims[1] / chunk_dims[1],
-                            global_dims[2] / chunk_dims[2]};
-}
-
-inline auto get_chunklist(int rank, const std::array<int, 3>& rank_dims, int num_chunks_per_rank)
-{
-  std::vector<int> id;
-  std::vector<int> oz;
-  std::vector<int> oy;
-  std::vector<int> ox;
-
-  id.reserve(static_cast<size_t>(num_chunks_per_rank));
-  oz.reserve(static_cast<size_t>(num_chunks_per_rank));
-  oy.reserve(static_cast<size_t>(num_chunks_per_rank));
-  ox.reserve(static_cast<size_t>(num_chunks_per_rank));
-
-  const int nx = rank_dims[2];
-  const int ny = rank_dims[1];
-
-  // global chunk id = cx*sx + cy*sy + cz*sz
-  const int sx = 1;
-  const int sy = nx;
-  const int sz = nx * ny;
-
-  for (int i = 0; i < num_chunks_per_rank; ++i) {
-    const int id_global = rank * num_chunks_per_rank + i;
-
-    // chunk coordinates
-    const int cx = (id_global / sx) % nx;
-    const int cy = (id_global / sy) % ny;
-    const int cz = (id_global / sz);
-
-    id.push_back(id_global);
-    oz.push_back(cz);
-    oy.push_back(cy);
-    ox.push_back(cx);
-  }
-
-  return std::make_tuple(std::move(id), std::move(oz), std::move(oy), std::move(ox));
-}
-
-inline auto get_chunkvec(const std::array<int, 3>& dims, const std::vector<int>& id,
-                         const std::vector<int>& oz, const std::vector<int>& oy,
-                         const std::vector<int>& ox)
-{
-  std::vector<std::shared_ptr<MockChunk>> chunkvec;
-  chunkvec.reserve(id.size());
-
-  for (size_t i = 0; i < id.size(); ++i) {
-    auto chunk = std::make_shared<MockChunk>();
-    chunk->set_id(id[i]);
-    chunk->set_dims({dims[0], dims[1], dims[2]});
-    chunk->set_offset({oz[i] * dims[0], oy[i] * dims[1], ox[i] * dims[2]});
-    chunkvec.push_back(std::move(chunk));
-  }
-
-  return chunkvec;
-}
-
-inline auto get_chunkvec(int rank, const std::array<int, 3>& chunk_dims,
-                         const std::array<int, 3>& rank_dims, int num_chunks_per_rank)
-{
-  const auto [cids, offz, offy, offx] = get_chunklist(rank, rank_dims, num_chunks_per_rank);
-  return get_chunkvec(chunk_dims, cids, offz, offy, offx);
-}
-
-inline auto get_index_and_chunkvec(int rank, const std::array<int, 3>& chunk_dims,
-                                   const std::array<int, 3>& global_dims, int num_chunks_per_rank)
-{
-  const auto rank_dims = get_rank_dims(chunk_dims, global_dims);
-
-  // check consistency
-  const int size = get_mpi_size();
-  assert(size * num_chunks_per_rank == (rank_dims[0] * rank_dims[1] * rank_dims[2]));
-
-  const int chunk_size = chunk_dims[0] * chunk_dims[1] * chunk_dims[2];
-
-  std::vector<int> index(chunk_size * num_chunks_per_rank);
-  auto             chunkvec = get_chunkvec(rank, chunk_dims, rank_dims, num_chunks_per_rank);
-
-  return std::make_tuple(std::move(index), std::move(chunkvec));
-}
 
 TEST_CASE("PetscUtils::setup_indexset_local", "[np=1]")
 {
