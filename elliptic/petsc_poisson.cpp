de832f2a15c2c6d505029a34e147de6280290927
Takanobu Amano <amano@eps.s.u-tokyo.ac.jp>
2026-01-26 14:59:28 +0900

    WIP: add poisson matrix helpers

--- elliptic/petsc_poisson.cpp
+++ elliptic/petsc_poisson.cpp
@@ -1,3 +1,5 @@
+#include "petsc_matrix_helpers.hpp"
+
 #include "petsc_poisson.hpp"
 
 #include <petscdmda.h>
@@ -61,45 +63,12 @@ int PetscPoisson1D::set_matrix()
   const float64 diag    = +2.0 * dx2_inv;
   const float64 ofdx    = -1.0 * dx2_inv;
 
-  DMDALocalInfo info;
-  DMDAGetLocalInfo(dm_obj, &info);
-
   if (matrix != nullptr) {
     MatDestroy(&matrix);
   }
   DMCreateMatrix(dm_obj, &matrix);
 
-  for (int ix = info.xs; ix < info.xs + info.xm; ++ix) {
-    MatStencil row;
-    MatStencil col[3];
-    float64    vals[3];
-    int        ncols = 0;
-
-    row.i = ix;
-    row.j = 0;
-    row.k = 0;
-    row.c = 0;
-
-    col[ncols]    = row;
-    vals[ncols++] = diag;
-
-    col[ncols].i  = ix - 1;
-    col[ncols].j  = 0;
-    col[ncols].k  = 0;
-    col[ncols].c  = 0;
-    vals[ncols++] = ofdx;
-
-    col[ncols].i  = ix + 1;
-    col[ncols].j  = 0;
-    col[ncols].k  = 0;
-    col[ncols].c  = 0;
-    vals[ncols++] = ofdx;
-
-    MatSetValuesStencil(matrix, 1, &row, ncols, col, vals, ADD_VALUES);
-  }
-
-  MatAssemblyBegin(matrix, MAT_FINAL_ASSEMBLY);
-  MatAssemblyEnd(matrix, MAT_FINAL_ASSEMBLY);
+  build_poisson_matrix_1d(matrix, dm_obj, diag, ofdx);
 
   set_nullspace();
 
@@ -119,59 +88,12 @@ int PetscPoisson2D::set_matrix()
   const float64 ofdx    = -1.0 * dx2_inv;
   const float64 ofdy    = -1.0 * dy2_inv;
 
-  DMDALocalInfo info;
-  DMDAGetLocalInfo(dm_obj, &info);
-
   if (matrix != nullptr) {
     MatDestroy(&matrix);
   }
   DMCreateMatrix(dm_obj, &matrix);
 
-  for (int iy = info.ys; iy < info.ys + info.ym; ++iy) {
-    for (int ix = info.xs; ix < info.xs + info.xm; ++ix) {
-      MatStencil row;
-      MatStencil col[5];
-      float64    vals[5];
-      int        ncols = 0;
-
-      row.i = ix;
-      row.j = iy;
-      row.k = 0;
-      row.c = 0;
-
-      col[ncols]    = row;
-      vals[ncols++] = diag;
-
-      col[ncols].i  = ix - 1;
-      col[ncols].j  = iy;
-      col[ncols].k  = 0;
-      col[ncols].c  = 0;
-      vals[ncols++] = ofdx;
-
-      col[ncols].i  = ix + 1;
-      col[ncols].j  = iy;
-      col[ncols].k  = 0;
-      col[ncols].c  = 0;
-      vals[ncols++] = ofdx;
-
-      col[ncols].i  = ix;
-      col[ncols].j  = iy - 1;
-      col[ncols].k  = 0;
-      col[ncols].c  = 0;
-      vals[ncols++] = ofdy;
-
-      col[ncols].i  = ix;
-      col[ncols].j  = iy + 1;
-      col[ncols].k  = 0;
-      col[ncols].c  = 0;
-      vals[ncols++] = ofdy;
-
-      MatSetValuesStencil(matrix, 1, &row, ncols, col, vals, ADD_VALUES);
-    }
-  }
-
-  MatAssemblyBegin(matrix, MAT_FINAL_ASSEMBLY);
-  MatAssemblyEnd(matrix, MAT_FINAL_ASSEMBLY);
+  build_poisson_matrix_2d(matrix, dm_obj, diag, ofdx, ofdy);
 
   set_nullspace();
 
@@ -193,83 +115,12 @@ int PetscPoisson3D::set_matrix()
   const float64 ofdy    = -1.0 * dy2_inv;
   const float64 ofdz    = -1.0 * dz2_inv;
 
-  DMDALocalInfo info;
-  DMDAGetLocalInfo(dm_obj, &info);
-
   if (matrix != nullptr) {
     MatDestroy(&matrix);
   }
   DMCreateMatrix(dm_obj, &matrix);
 
-  for (int iz = info.zs; iz < info.zs + info.zm; ++iz) {
-    for (int iy = info.ys; iy < info.ys + info.ym; ++iy) {
-      for (int ix = info.xs; ix < info.xs + info.xm; ++ix) {
-        MatStencil row;
-        MatStencil col[7];
-        float64    vals[7];
-        int        ncols = 0;
-
-        row.i = ix;
-        row.j = iy;
-        row.k = iz;
-        row.c = 0;
-
-        col[ncols].i = ix;
-        col[ncols].j = iy;
-        col[ncols].k = iz;
-        col[ncols].c = 0;
-        vals[ncols]  = diag;
-        ncols++;
-
-        col[ncols].i = ix - 1;
-        col[ncols].j = iy;
-        col[ncols].k = iz;
-        col[ncols].c = 0;
-        vals[ncols]  = ofdx;
-        ncols++;
-
-        col[ncols].i = ix + 1;
-        col[ncols].j = iy;
-        col[ncols].k = iz;
-        col[ncols].c = 0;
-        vals[ncols]  = ofdx;
-        ncols++;
-
-        col[ncols].i = ix;
-        col[ncols].j = iy - 1;
-        col[ncols].k = iz;
-        col[ncols].c = 0;
-        vals[ncols]  = ofdy;
-        ncols++;
-
-        col[ncols].i = ix;
-        col[ncols].j = iy + 1;
-        col[ncols].k = iz;
-        col[ncols].c = 0;
-        vals[ncols]  = ofdy;
-        ncols++;
-
-        col[ncols].i = ix;
-        col[ncols].j = iy;
-        col[ncols].k = iz - 1;
-        col[ncols].c = 0;
-        vals[ncols]  = ofdz;
-        ncols++;
-
-        col[ncols].i = ix;
-        col[ncols].j = iy;
-        col[ncols].k = iz + 1;
-        col[ncols].c = 0;
-        vals[ncols]  = ofdz;
-        ncols++;
-
-        MatSetValuesStencil(matrix, 1, &row, ncols, col, vals, ADD_VALUES);
-      }
-    }
-  }
-
-  MatAssemblyBegin(matrix, MAT_FINAL_ASSEMBLY);
-  MatAssemblyEnd(matrix, MAT_FINAL_ASSEMBLY);
+  build_poisson_matrix_3d(matrix, dm_obj, diag, ofdx, ofdy, ofdz);
 
   set_nullspace();
 
