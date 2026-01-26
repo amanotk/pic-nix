8dbd15776ad49df3f902b19b9be1527bcb7a0f1d
Takanobu Amano <amano@eps.s.u-tokyo.ac.jp>
2026-01-26 15:42:24 +0900

    WIP: PicPoisson inherit PetscInterface

--- pic/pic_poisson.cpp
+++ pic/pic_poisson.cpp
@@ -4,75 +4,139 @@
 #include <array>
 #include <cassert>
 
-PicPoisson::PicPoisson(const nix::Dims3D& global_dims, float64 delh) : global_dims_(global_dims)
-{
-  const bool is_1d = (global_dims_[0] == 1) && (global_dims_[1] == 1);
-  const bool is_2d = (global_dims_[0] == 1) && (global_dims_[1] > 1);
-  const bool is_3d = (global_dims_[0] > 1) && (global_dims_[1] > 1);
+#include "elliptic/petsc_matrix_helpers.hpp"
 
-  if (is_1d) {
-    solver_ = std::make_unique<SolverAdapter<elliptic::PetscPoisson1D>>(global_dims_, delh);
-  } else if (is_2d) {
-    solver_ = std::make_unique<SolverAdapter<elliptic::PetscPoisson2D>>(global_dims_, delh);
-  } else if (is_3d) {
-    solver_ = std::make_unique<SolverAdapter<elliptic::PetscPoisson3D>>(global_dims_, delh);
-  } else {
-    ERROR << tfm::format("Invalid global dimensions for PicPoisson: %d %d %d", global_dims_[0],
-                         global_dims_[1], global_dims_[2]);
-    MPI_Abort(MPI_COMM_WORLD, -1);
-  }
+#include <petscdmda.h>
+#include <petscmat.h>
+
+using namespace nix::typedefs;
+
+PicPoisson::PicPoisson(const nix::Dims3D& global_dims, float64 delh)
+    : PetscInterface(global_dims), global_dims_(global_dims), accessor_(), delx(delh), dely(delh),
+      delz(delh)
+{
+  setup();
 }
 
 int PicPoisson::update_mapping(const ChunkVec& chunks)
 {
   accessor_.set_chunks(chunks);
-  return solver_->update_mapping(accessor_);
+  return PetscInterface::update_mapping(accessor_);
 }
 
 int PicPoisson::scatter_rhs(const ChunkVec& chunks)
 {
   update_mapping(chunks);
-  solver_->copy_chunk_to_src(accessor_);
-  solver_->scatter_forward_begin();
-  solver_->scatter_forward_end();
+  copy_chunk_to_src(accessor_);
+  scatter_forward_begin();
+  scatter_forward_end();
   return 0;
 }
 
 int PicPoisson::copy_rhs_to_solution()
 {
-  return solver_->copy_rhs_to_solution();
+  return VecCopy(vector_src_g, vector_sol_g);
 }
 
 int PicPoisson::scatter_solution_to_chunks(const ChunkVec& chunks)
 {
   update_mapping(chunks);
-  solver_->scatter_reverse_begin();
-  solver_->scatter_reverse_end();
-  solver_->copy_sol_to_chunk(accessor_);
+  scatter_reverse_begin();
+  scatter_reverse_end();
+  copy_sol_to_chunk(accessor_);
   return 0;
 }
 
 int PicPoisson::solve(const ChunkVec& chunks)
 {
   update_mapping(chunks);
-  solver_->copy_chunk_to_src(accessor_);
-  solver_->scatter_forward_begin();
-  solver_->scatter_forward_end();
-  int status = solver_->solve();
-  solver_->scatter_reverse_begin();
-  solver_->scatter_reverse_end();
-  solver_->copy_sol_to_chunk(accessor_);
+  copy_chunk_to_src(accessor_);
+  scatter_forward_begin();
+  scatter_forward_end();
+  const int status = solve();
+  scatter_reverse_begin();
+  scatter_reverse_end();
+  copy_sol_to_chunk(accessor_);
   return status;
+}
+
+int PicPoisson::solve(elliptic::ChunkAccessor& accessor)
+{
+  return solve();
+}
+
+int PicPoisson::solve()
+{
+  PetscErrorCode ierr = KSPSolve(ksp_obj, vector_src_g, vector_sol_g);
+  if (ierr != 0) {
+    ERROR << "KSPSolve failed with error code: " << ierr << std::endl;
+  }
+  return ierr;
 }
 
 int PicPoisson::set_option(const json& config)
 {
-  return solver_->set_option(config);
+  return PetscInterface::set_option(config);
 }
 
 float64 PicPoisson::get_residual_norm()
 {
-  return solver_->get_residual_norm();
+  Vec       vector_res_g;
+  PetscReal res_norm;
+  PetscReal src_norm;
+
+  VecDuplicate(vector_src_g, &vector_res_g);
+  MatMult(matrix, vector_sol_g, vector_res_g);
+  VecAYPX(vector_res_g, -1.0, vector_src_g);
+  VecNorm(vector_res_g, NORM_2, &res_norm);
+  VecNorm(vector_src_g, NORM_2, &src_norm);
+  VecDestroy(&vector_res_g);
+
+  return static_cast<float64>(res_norm / (src_norm + 1.0e-32));
+}
+
+int PicPoisson::set_matrix()
+{
+  const bool    is_1d   = (global_dims_[0] == 1) && (global_dims_[1] == 1);
+  const bool    is_2d   = (global_dims_[0] == 1) && (global_dims_[1] > 1);
+  const bool    is_3d   = (global_dims_[0] > 1) && (global_dims_[1] > 1);
+  const float64 dx2_inv = 1.0 / (delx * delx);
+  const float64 dy2_inv = 1.0 / (dely * dely);
+  const float64 dz2_inv = 1.0 / (delz * delz);
+  const float64 ofdx    = -1.0 * dx2_inv;
+  const float64 ofdy    = -1.0 * dy2_inv;
+  const float64 ofdz    = -1.0 * dz2_inv;
+  const float64 diag_1d = +2.0 * dx2_inv;
+  const float64 diag_2d = +2.0 * dx2_inv + 2.0 * dy2_inv;
+  const float64 diag_3d = +2.0 * dx2_inv + 2.0 * dy2_inv + 2.0 * dz2_inv;
+
+  if (matrix != nullptr) {
+    MatDestroy(&matrix);
+  }
+  DMCreateMatrix(dm_obj, &matrix);
+
+  if (is_1d) {
+    elliptic::build_poisson_matrix_1d(matrix, dm_obj, diag_1d, ofdx);
+  } else if (is_2d) {
+    elliptic::build_poisson_matrix_2d(matrix, dm_obj, diag_2d, ofdx, ofdy);
+  } else if (is_3d) {
+    elliptic::build_poisson_matrix_3d(matrix, dm_obj, diag_3d, ofdx, ofdy, ofdz);
+  } else {
+    ERROR << tfm::format("Invalid global dimensions for PicPoisson: %d %d %d", global_dims_[0],
+                         global_dims_[1], global_dims_[2]);
+    MPI_Abort(MPI_COMM_WORLD, -1);
+  }
+
+  set_nullspace();
+  return 0;
+}
+
+void PicPoisson::set_nullspace()
+{
+  MatNullSpace ns;
+  MatNullSpaceCreate(PETSC_COMM_WORLD, PETSC_TRUE, 0, nullptr, &ns);
+  MatSetNullSpace(matrix, ns);
+  MatNullSpaceDestroy(&ns);
 }
 
 void PicPoisson::PicChunkAccessor::set_chunks(const ChunkVec& chunks)
--- pic/pic_poisson.hpp
+++ pic/pic_poisson.hpp
@@ -6,10 +6,10 @@
 #include "pic_chunk.hpp"
 
 #include "elliptic/chunk_accessor.hpp"
-#include "elliptic/petsc_poisson.hpp"
+#include "elliptic/petsc_interface.hpp"
 #include <petscvec.h>
 
-class PicPoisson
+class PicPoisson : public elliptic::PetscInterface
 {
 public:
   using ChunkVec = std::vector<PicChunk*>;
@@ -23,6 +23,9 @@ public:
   int     solve(const ChunkVec& chunks);
   int     set_option(const json& config);
   float64 get_residual_norm();
+
+protected:
+  int set_matrix() override;
 
 private:
   class PicChunkAccessor : public elliptic::ChunkAccessor
@@ -47,92 +50,14 @@ private:
     int         chunk_size_;
   };
 
-  struct SolverBase {
-    virtual ~SolverBase()                                                = default;
-    virtual int     update_mapping(elliptic::ChunkAccessor& accessor)    = 0;
-    virtual int     copy_chunk_to_src(elliptic::ChunkAccessor& accessor) = 0;
-    virtual int     copy_sol_to_chunk(elliptic::ChunkAccessor& accessor) = 0;
-    virtual int     scatter_forward_begin()                              = 0;
-    virtual int     scatter_forward_end()                                = 0;
-    virtual int     scatter_reverse_begin()                              = 0;
-    virtual int     scatter_reverse_end()                                = 0;
-    virtual int     solve()                                              = 0;
-    virtual int     set_option(const json& config)                       = 0;
-    virtual float64 get_residual_norm()                                  = 0;
-    virtual int     copy_rhs_to_solution()                               = 0;
-  };
-
-  template <typename T>
-  class SolverAdapter : public T, public SolverBase
-  {
-  public:
-    using T::T;
-    using elliptic::PetscInterface::scatter_forward_begin;
-    using elliptic::PetscInterface::scatter_forward_end;
-    using elliptic::PetscInterface::scatter_reverse_begin;
-    using elliptic::PetscInterface::scatter_reverse_end;
-
-    int update_mapping(elliptic::ChunkAccessor& accessor) override
-    {
-      return T::update_mapping(accessor);
-    }
-
-    int copy_chunk_to_src(elliptic::ChunkAccessor& accessor) override
-    {
-      return T::copy_chunk_to_src(accessor);
-    }
-
-    int copy_sol_to_chunk(elliptic::ChunkAccessor& accessor) override
-    {
-      return T::copy_sol_to_chunk(accessor);
-    }
-
-    int scatter_forward_begin() override
-    {
-      return T::scatter_forward_begin();
-    }
-
-    int scatter_forward_end() override
-    {
-      return T::scatter_forward_end();
-    }
-
-    int scatter_reverse_begin() override
-    {
-      return T::scatter_reverse_begin();
-    }
-
-    int scatter_reverse_end() override
-    {
-      return T::scatter_reverse_end();
-    }
-
-    int solve() override
-    {
-      return T::solve();
-    }
-
-    int set_option(const json& config) override
-    {
-      return T::set_option(config);
-    }
-
-    float64 get_residual_norm() override
-    {
-      return T::get_residual_norm();
-    }
-
-    int copy_rhs_to_solution() override
-    {
-      return VecCopy(this->vector_src_g, this->vector_sol_g);
-    }
-  };
-
-  using SolverPtr = std::unique_ptr<SolverBase>;
-
   nix::Dims3D      global_dims_;
-  SolverPtr        solver_;
   PicChunkAccessor accessor_;
+  float64          delx;
+  float64          dely;
+  float64          delz;
+  void             set_nullspace();
+  int              solve() override;
+  int              solve(elliptic::ChunkAccessor& accessor) override;
 };
 
 #endif //_PIC_POISSON_HPP_
