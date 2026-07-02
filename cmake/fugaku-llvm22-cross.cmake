beacfa50fdf80251741a744dddc43972fed128f3
Takanobu Amano <amano@eps.s.u-tokyo.ac.jp>
2026-07-02 11:41:28 +0900

    Fix typos and clarify env setup comments in Fugaku LLVM-22 CMake cache

--- cmake/fugaku-llvm22-cross.cmake
+++ cmake/fugaku-llvm22-cross.cmake
@@ -1,9 +1,16 @@
 #
 # LLVM-22.1.4 cross compiler on Fugaku Login Node
 #
-# Seting up the environment with the following commands is needed:
+# On login node, setting up the environment with the following commands
+# is needed for compilation:
 #
 # $ module load LLVM/llvmorg-22.1.0
+#
+# In a job script for the scheduler (PJM), the following is needed:
+#
+# module purge
+# module load LLVM/llvmorg-22.1.0
+# llio_transfer `find ${LLVM_BASEDIR}/lib64 -type f -name \*.so\*`
 #
 set(CMAKE_SYSTEM_NAME Fugaku)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
