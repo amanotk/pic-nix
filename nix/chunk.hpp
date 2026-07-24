fa6d07778a5fff447d67828eabc8ce785bf33e08
Takanobu Amano <amano@eps.s.u-tokyo.ac.jp>
2026-07-25 00:25:35 +0900

    refactor: centralize array types and utilities in dedicated headers
    - Add nix/array_types.hpp with Array1D<T>–Array5D<T> aliases and
      generalized FixedArray3D, FixedArray4D structs
    - Add nix/array_utils.hpp with backend-agnostic array traits and
      utility functions extracted from nix/nix.hpp
    - Migrate pic/pic_chunk.hpp and other consumers to use nix::ArrayND
      in place of explicit xt::xtensor
    - Replace xt::xtensor_fixed in pic/pic_application.hpp with
      nix::FixedArray4D
    - Remove local FixedArray3D definition from nix/chunk.hpp

--- nix/chunk.hpp
+++ nix/chunk.hpp
@@ -2,43 +2,12 @@
 #ifndef _CHUNK_HPP_
 #define _CHUNK_HPP_
 
+#include "array_types.hpp"
 #include "buffer.hpp"
 #include "debug.hpp"
 #include "nix.hpp"
 
 NIX_NAMESPACE_BEGIN
-
-template <typename T>
-struct FixedArray3D {
-  static constexpr int N = 27;
-
-  std::array<T, N> data_{};
-
-  T& operator()(int z, int y, int x)
-  {
-    return data_[z * 9 + y * 3 + x];
-  }
-  const T& operator()(int z, int y, int x) const
-  {
-    return data_[z * 9 + y * 3 + x];
-  }
-  T* data()
-  {
-    return data_.data();
-  }
-  const T* data() const
-  {
-    return data_.data();
-  }
-  constexpr size_t size() const
-  {
-    return N;
-  }
-  void fill(const T& v)
-  {
-    data_.fill(v);
-  }
-};
 
 ///
 /// @brief Base class for 3D Chunk
@@ -49,10 +18,10 @@ public:
   static constexpr int nbsize = 27;
 
   struct MpiBuffer; ///< forward declaration
-  using IntArray     = FixedArray3D<int>;
-  using Comm         = FixedArray3D<MPI_Comm>;
-  using Request      = FixedArray3D<MPI_Request>;
-  using Datatype     = FixedArray3D<MPI_Datatype>;
+  using IntArray     = FixedArray3D<int, 3, 3, 3>;
+  using Comm         = FixedArray3D<MPI_Comm, 3, 3, 3>;
+  using Request      = FixedArray3D<MPI_Request, 3, 3, 3>;
+  using Datatype     = FixedArray3D<MPI_Datatype, 3, 3, 3>;
   using MpiBufferPtr = std::shared_ptr<MpiBuffer>;
   using MpiBufferVec = std::vector<MpiBufferPtr>;
 
